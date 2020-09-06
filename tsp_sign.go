8c9eb2f56b14f4c0493d02d4a2777f7f145b2eb8
danikarik <daniyar@hey.com>
2020-09-06 22:31:40 +0600

    Add TSPSign support

---
+++ tsp_sign.go
@@ -0,0 +1,37 @@
+package ncanode
+
+type TSPSignRequest struct {
+	Raw              string        `json:"raw"`
+	UseTsaPolicy     Policy        `json:"useTsaPolicy,omitempty"`
+	TSPHashAlgorithm HashAlgorithm `json:"tspHashAlgorithm,omitempty"`
+}
+
+type TSPSignResponse struct {
+	APIResponse
+	Result struct {
+		TSP string `json:"tsp"`
+	} `json:"result"`
+}
+
+func (c *Client) TSPSign(raw string, policy Policy, alg HashAlgorithm) (*TSPSignResponse, error) {
+	if raw == "" {
+		return nil, ErrInvalidRequestBody
+	}
+
+	body := APIRequest{
+		Version: _v1,
+		Method:  "TSP.sign",
+		Params: TSPSignRequest{
+			Raw:              raw,
+			UseTsaPolicy:     policy,
+			TSPHashAlgorithm: alg,
+		},
+	}
+
+	var reply TSPSignResponse
+	if err := c.call(body, &reply); err != nil {
+		return nil, err
+	}
+
+	return &reply, nil
+}
