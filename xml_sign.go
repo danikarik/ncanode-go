62a04ab666b39b069179a261affdf256bc64ebda
danikarik <daniyar@hey.com>
2020-09-06 22:01:13 +0600

    Add XMLSign support

---
+++ xml_sign.go
@@ -0,0 +1,77 @@
+package ncanode
+
+type Policy string
+
+const (
+	TSAGOSTPOLICY   Policy = "TSA_GOST_POLICY"
+	TSAGOSTGTPOLICY Policy = "TSA_GOSTGT_POLICY"
+)
+
+type HashAlgorithm string
+
+const (
+	MD5         HashAlgorithm = "MD5"
+	SHA1        HashAlgorithm = "SHA1"
+	SHA224      HashAlgorithm = "SHA224"
+	SHA256      HashAlgorithm = "SHA256"
+	SHA384      HashAlgorithm = "SHA384"
+	SHA512      HashAlgorithm = "SHA512"
+	RIPEMD128   HashAlgorithm = "RIPEMD128"
+	RIPEMD160   HashAlgorithm = "RIPEMD160"
+	RIPEMD256   HashAlgorithm = "RIPEMD256"
+	GOST34311GT HashAlgorithm = "GOST34311GT"
+	GOST34311   HashAlgorithm = "GOST34311"
+)
+
+type TSPConfig struct {
+	Enabled       bool
+	Policy        Policy
+	HashAlgorithm HashAlgorithm
+}
+
+type XMLSignRequest struct {
+	P12              string        `json:"p12"`
+	Password         string        `json:"password"`
+	XML              string        `json:"xml"`
+	CreateTsp        bool          `json:"createTsp,omitempty"`
+	UseTsaPolicy     Policy        `json:"useTsaPolicy,omitempty"`
+	TSPHashAlgorithm HashAlgorithm `json:"tspHashAlgorithm,omitempty"`
+}
+
+type XMLSignResponse struct {
+	APIResponse
+	Result struct {
+		XML string `json:"xml"`
+		TSP string `json:"tsp"`
+	} `json:"result"`
+}
+
+func (c *Client) XMLSign(p12, password, xml string, config *TSPConfig) (*XMLSignResponse, error) {
+	if p12 == "" || password == "" || xml == "" {
+		return nil, ErrInvalidRequestBody
+	}
+
+	if config == nil {
+		config = &TSPConfig{}
+	}
+
+	body := APIRequest{
+		Version: _v1,
+		Method:  "XML.sign",
+		Params: XMLSignRequest{
+			P12:              p12,
+			Password:         password,
+			XML:              xml,
+			CreateTsp:        config.Enabled,
+			UseTsaPolicy:     config.Policy,
+			TSPHashAlgorithm: config.HashAlgorithm,
+		},
+	}
+
+	var reply XMLSignResponse
+	if err := c.call(body, &reply); err != nil {
+		return nil, err
+	}
+
+	return &reply, nil
+}
