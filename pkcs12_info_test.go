e534e75a46ba4928b09586171444dc77f357cd2e
danikarik <daniyar@hey.com>
2020-09-09 14:54:57 +0600

    Refactor X509Info

--- pkcs12_info_test.go
+++ pkcs12_info_test.go
@@ -14,7 +14,6 @@ func TestPKCS12Info(t *testing.T) {
 	testCases := []struct {
 		Name           string
 		Path           string
-		Password       string
 		VerifyOCSP     bool
 		VerifyCRL      bool
 		ExpectedResult bool
@@ -22,19 +21,16 @@ func TestPKCS12Info(t *testing.T) {
 		{
 			Name:           "Personal/Active/Auth",
 			Path:           "personal/active/AUTH_RSA256_b2c70a9ff7a5dc59de0ccc3c0ddde3437cc1f12d.p12",
-			Password:       _defaultPassword,
 			ExpectedResult: true,
 		},
 		{
 			Name:           "Personal/Active/Sign",
 			Path:           "personal/active/RSA256_11c707bd54cbfcccb3815e39c4eb57b1dc7dfea9.p12",
-			Password:       _defaultPassword,
 			ExpectedResult: true,
 		},
 		{
 			Name:           "Personal/Revoked/Auth",
 			Path:           "personal/revoked/AUTH_RSA256_60d5a0346dd52fb5f3b9148e6cfbcd6cf323d119.p12",
-			Password:       _defaultPassword,
 			VerifyOCSP:     true,
 			VerifyCRL:      true,
 			ExpectedResult: false,
@@ -42,7 +38,6 @@ func TestPKCS12Info(t *testing.T) {
 		{
 			Name:           "Personal/Revoked/Sign",
 			Path:           "personal/revoked/RSA256_346a7e57c2995259140b6fc375b6ff3bba7e852f.p12",
-			Password:       _defaultPassword,
 			VerifyOCSP:     true,
 			VerifyCRL:      true,
 			ExpectedResult: false,
@@ -50,19 +45,16 @@ func TestPKCS12Info(t *testing.T) {
 		{
 			Name:           "Organization/Active/Auth",
 			Path:           "organization/active/AUTH_RSA256_d682726b55b3e62600ea8fe1c74e75fc96f47790.p12",
-			Password:       _defaultPassword,
 			ExpectedResult: true,
 		},
 		{
 			Name:           "Organization/Active/Sign",
 			Path:           "organization/active/GOSTKNCA_13a3741b52d7bf860aab199b56fad1f4652e357b.p12",
-			Password:       _defaultPassword,
 			ExpectedResult: true,
 		},
 		{
 			Name:           "Organization/Revoked/Auth",
 			Path:           "organization/revoked/AUTH_RSA256_7d6d313ac5bf7367a2a69f28607a8deb80dd3ba9.p12",
-			Password:       _defaultPassword,
 			VerifyOCSP:     true,
 			VerifyCRL:      true,
 			ExpectedResult: false,
@@ -70,7 +62,6 @@ func TestPKCS12Info(t *testing.T) {
 		{
 			Name:           "Organization/Revoked/Sign",
 			Path:           "organization/revoked/GOSTKNCA_c372b7809440fcb681051bbc89ea2f089fd2fe16.p12",
-			Password:       _defaultPassword,
 			VerifyOCSP:     true,
 			VerifyCRL:      true,
 			ExpectedResult: false,
@@ -82,9 +73,17 @@ func TestPKCS12Info(t *testing.T) {
 			p12, err := base64content(tc.Path)
 			require.NoError(t, err)
 
-			resp, err := client.PKCS12Info(p12, tc.Password, tc.VerifyOCSP, tc.VerifyCRL)
+			resp, err := client.PKCS12Info(p12, _defaultPassword, tc.VerifyOCSP, tc.VerifyCRL)
 			require.NoError(t, err)
 			require.Equal(t, tc.ExpectedResult, resp.Result.Valid)
+
+			if tc.VerifyOCSP {
+				require.NotNil(t, resp.Result.OCSP)
+			}
+
+			if tc.VerifyCRL {
+				require.NotNil(t, resp.Result.CRL)
+			}
 		})
 	}
 }
--- x509_info_test.go
+++ x509_info_test.go
@@ -12,29 +12,50 @@ func TestX509Info(t *testing.T) {
 	require.NoError(t, err)
 
 	testCases := []struct {
-		Name       string
-		Cert       string
-		VerifyOCSP bool
-		VerifyCRL  bool
+		Name           string
+		Path           string
+		VerifyOCSP     bool
+		VerifyCRL      bool
+		ExpectedResult bool
 	}{
 		{
-			Name:       "Default",
-			Cert:       "MIIGZTCCBE2gAwIBAgIUFX1cSXpU/SdXs4r74PS8YFuVbAowDQYJKoZIhvcNAQELBQAwUjELMAkGA1UEBhMCS1oxQzBBBgNVBAMMOtKw0JvQotCi0KvSmiDQmtCj05jQm9CQ0J3QlNCr0KDQo9Co0Ksg0J7QoNCi0JDQm9Cr0pogKFJTQSkwHhcNMTgwODIyMTIxMTM2WhcNMTkwODIyMTIxMTM2WjCBpzEeMBwGA1UEAwwV0KLQldCh0KLQntCSINCi0JXQodCiMRUwEwYDVQQEDAzQotCV0KHQotCe0JIxGDAWBgNVBAUTD0lJTjEyMzQ1Njc4OTAxMTELMAkGA1UEBhMCS1oxFTATBgNVBAcMDNCQ0JvQnNCQ0KLQqzEVMBMGA1UECAwM0JDQm9Cc0JDQotCrMRkwFwYDVQQqDBDQotCV0KHQotCe0JLQmNCnMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtKWLOJf9qCqA6EO/SVtiMuPZ8q3Sg2RjO0dWXqKQRP7BWhIyMucMv+WmpRs8RuJ987Hm3B/JszSdiPrmtA9BpIERKphRwp3n4QR6pfLUBEp+5QNetNsv+dbiPcefWCzgJZCqEZVbPvSkiFH20y13YQ2FhEBUp4lLOqydBD2CsDVoTusvLanEgR+AdziJPq2+iXwhttpNPShKRTXGbGkxUa4P7YMUCUqWstR7svLaJqxKDMhaR7MpEt56a2pfntm5oFxKNFoBQjRXKbiBNIKciMRAeznjezv9ZA98WzWPIMuWzi38fPW5X7IVqa7ZbAFWvZIHWJmrl57uKGBNd9EUewIDAQABo4IB2zCCAdcwDgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsGAQUFBwMCBggqgw4DAwQBATAPBgNVHSMECDAGgARbanQRMB0GA1UdDgQWBBRrNhuGTGeWAbZS/jh/YfzZMDwDJzBeBgNVHSAEVzBVMFMGByqDDgMDAgQwSDAhBggrBgEFBQcCARYVaHR0cDovL3BraS5nb3Yua3ovY3BzMCMGCCsGAQUFBwICMBcMFWh0dHA6Ly9wa2kuZ292Lmt6L2NwczBWBgNVHR8ETzBNMEugSaBHhiFodHRwOi8vY3JsLnBraS5nb3Yua3ovbmNhX3JzYS5jcmyGImh0dHA6Ly9jcmwxLnBraS5nb3Yua3ovbmNhX3JzYS5jcmwwWgYDVR0uBFMwUTBPoE2gS4YjaHR0cDovL2NybC5wa2kuZ292Lmt6L25jYV9kX3JzYS5jcmyGJGh0dHA6Ly9jcmwxLnBraS5nb3Yua3ovbmNhX2RfcnNhLmNybDBiBggrBgEFBQcBAQRWMFQwLgYIKwYBBQUHMAKGImh0dHA6Ly9wa2kuZ292Lmt6L2NlcnQvbmNhX3JzYS5jZXIwIgYIKwYBBQUHMAGGFmh0dHA6Ly9vY3NwLnBraS5nb3Yua3owDQYJKoZIhvcNAQELBQADggIBACy0Lxj0D/q3SwUz0X9BICyKPw/U6sXmedqUcrghzZuT9ojnUp9w7g4ndZOKTRRxQyLiUYb9neJ3SGVuF/XYcs7Ovrp5RGNNHuVUR8bQz9cbWd/O2qRUY6qlg4ZSjYsjFYaQm8o+uO56PuqWG125O7XNUdAUHNBc2hUrrngGKU0FKxlBygxLpvTf4I9q3QA0PJ6MnHrUKlor4sRGar4hMJCbrxeMG4pv3Jx/r9fsKy7f+yZeQo3T4XAIXmUTXF8UC3HtIroxAP6yEoEhG76oS3qvYc1K/krI48ju5VYxmzEabNqRhiiEBpocIwCqFLLo9x3CKuUkuA7pwEib4YcCNxCTucCtd9x8dGgZRNffJV4de/Aja/VP84q8rxmcyogbUQzvPb+2/zKRh6cxYxnRsuL4wWUV+fxp/usy0mJMboQF7IcRFe1fXosU0RWYmKHITOCDbs0NKxTn7TSxEKMYdJN6YngCmKlmwR/+AfxhN1QMSQpU/m8Glwl+f5wZIL5MQJVhrrWIteh0tnb+OuDQHz4g2vmD2xq5jUQDFIrjXOdy4zToqM6tirt3nGDsblWgcgsPac50FLT1+um7W26UsmtZ9/wXvkxYC9kL5gUX53VD/bcKki8fogjrNoYEZiORRqmwvZ5EVe4w3Hfb7YCnc3NzhhIg6hqmzumXNCgLCt2q",
-			VerifyOCSP: false,
-			VerifyCRL:  false,
+			Name:           "Personal/Active/Auth",
+			Path:           "personal/active/AUTH_RSA256_b2c70a9ff7a5dc59de0ccc3c0ddde3437cc1f12d.cer",
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Personal/Active/Sign",
+			Path:           "personal/active/RSA256_11c707bd54cbfcccb3815e39c4eb57b1dc7dfea9.cer",
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Organization/Active/Auth",
+			Path:           "organization/active/AUTH_RSA256_d682726b55b3e62600ea8fe1c74e75fc96f47790.cer",
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Organization/Active/Sign",
+			Path:           "organization/active/GOSTKNCA_13a3741b52d7bf860aab199b56fad1f4652e357b.cer",
+			ExpectedResult: true,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			resp, err := client.X509Info(tc.Cert, tc.VerifyOCSP, tc.VerifyCRL)
+			cert, err := base64content(tc.Path)
 			require.NoError(t, err)
-			require.False(t, resp.Result.Valid)
-			require.Equal(t, ncanode.KeyUsageAuth, resp.Result.KeyUsage)
-			require.Equal(t, "122684438670642568061334282296011886211357830154", resp.Result.SerialNumber)
 
-			t.Log(resp.Result.OCSP)
-			t.Log(resp.Result.CRL)
+			resp, err := client.X509Info(cert, tc.VerifyOCSP, tc.VerifyCRL)
+			require.NoError(t, err)
+			require.Equal(t, tc.ExpectedResult, resp.Result.Valid)
+
+			if tc.VerifyOCSP {
+				require.NotNil(t, resp.Result.OCSP)
+			}
+
+			if tc.VerifyCRL {
+				require.NotNil(t, resp.Result.CRL)
+			}
 		})
 	}
 }
